703d197b12e08d38566d4ae779d1a63a9f1267d2
yhhaiua <471537867@qq.com>
2017-10-17 12:32:31 +0800

    yhhaiua
    common Add

---
+++ common/gjson/gjson.go
@@ -0,0 +1,139 @@
+//对json文件的操作
+
+package gjson
+
+import (
+	"encoding/json"
+)
+
+//Js json数据结构体
+type Js struct {
+	mdata interface{}
+}
+
+//NewJSONString 通过string创建json结构
+func NewJSONString(data string) (*Js, error) {
+	j := new(Js)
+	var f interface{}
+	err := json.Unmarshal([]byte(data), &f)
+	if err != nil {
+		return j, err
+	}
+	j.mdata = f
+	return j, err
+}
+
+//NewJSONByte 通过[]byte创建json结构
+func NewJSONByte(data []byte) (*Js, error) {
+	j := new(Js)
+	var f interface{}
+	err := json.Unmarshal(data, &f)
+	if err != nil {
+		return j, err
+	}
+	j.mdata = f
+	return j, err
+}
+
+//NewGet 通过json结构创建json结构
+func NewGet(j *Js, key string) *Js {
+	value := new(Js)
+	value.mdata = j.get(key)
+	return value
+}
+
+//NewGetindex 通过json结构创建json结构
+func NewGetindex(j *Js, i int) *Js {
+	value := new(Js)
+	value.mdata = j.getlist(i)
+	return value
+}
+
+//Getnum 获取[]数组数量
+func (j *Js) Getnum() int {
+	if m, ok := (j.mdata).([]interface{}); ok {
+		return len(m)
+	}
+	return 0
+}
+
+//IsValid 判断json结构是否有数据
+func (j *Js) IsValid() bool {
+	if nil == j.mdata {
+		return false
+	}
+	return true
+}
+
+//Getuint16 通过key获取uint16
+func (j *Js) Getuint16(key string) uint16 {
+	data := j.get(key)
+	if data != nil {
+		if m, ok := data.(float64); ok {
+			return uint16(m)
+		}
+	}
+	return 0
+}
+
+//Getstring 通过key获取string
+func (j *Js) Getstring(key string) string {
+	data := j.get(key)
+	if data != nil {
+		if m, ok := data.(string); ok {
+			return m
+		}
+	}
+	return ""
+}
+
+//Getint32 通过key获取int32
+func (j *Js) Getint32(key string) int32 {
+	data := j.get(key)
+	if data != nil {
+		if m, ok := data.(float64); ok {
+			return int32(m)
+		}
+	}
+	return 0
+}
+
+//Getint 通过key获取int
+func (j *Js) Getint(key string) int {
+	data := j.get(key)
+	if data != nil {
+		if m, ok := data.(float64); ok {
+			return int(m)
+		}
+	}
+	return 0
+}
+
+//Getbool 通过key获取bool
+func (j *Js) Getbool(key string) bool {
+	data := j.get(key)
+	if data != nil {
+		if m, ok := data.(bool); ok {
+			return m
+		}
+	}
+	return false
+}
+
+func (j *Js) get(key string) interface{} {
+	if m, ok := (j.mdata).(map[string]interface{}); ok {
+		if data, oki := m[key]; oki {
+			return data
+		}
+	}
+	return nil
+}
+func (j *Js) getlist(i int) interface{} {
+
+	if m, ok := (j.mdata).([]interface{}); ok {
+		if i < len(m) {
+			return m[i]
+		}
+	}
+	return nil
+}
---
+++ common/redisuse.go
@@ -0,0 +1,178 @@
+package common
+
+import (
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+// RedisPool Redis连接结构
+type RedisPool struct {
+	p        *redis.Pool // redis connection pool
+	conninfo string
+	dbNum    int
+}
+
+func newRedis() *RedisPool {
+	newRedis := new(RedisPool)
+
+	return newRedis
+}
+
+func (rc *RedisPool) do(commandName string, args ...interface{}) (reply interface{}, err error) {
+	c := rc.p.Get()
+	defer c.Close()
+
+	return c.Do(commandName, args...)
+}
+
+// Zadd redis有序集合添加.
+func (rc *RedisPool) Zadd(keyname string, updatedate string) error {
+
+	cur := time.Now()
+	timestamp := cur.UnixNano()
+	var err error
+	if _, err = rc.do("ZADD", keyname, timestamp, updatedate); err != nil {
+		return err
+	}
+	return err
+}
+
+// Zrange redis有序集合获取
+func (rc *RedisPool) Zrange(keyname string) ([]string, error) {
+
+	return redis.Strings(rc.do("ZRANGE", keyname, 0, -1, "WITHSCORES"))
+}
+
+// Get redis获取value string.
+func (rc *RedisPool) Get(key string) (string, error) {
+
+	return redis.String(rc.do("GET", key))
+}
+
+// GetInt64 redis获取value int64.
+func (rc *RedisPool) GetInt64(key string) (int64, error) {
+
+	return redis.Int64(rc.do("GET", key))
+}
+
+// GetMulti redis获取多组value.
+func (rc *RedisPool) GetMulti(keys []string) []interface{} {
+	size := len(keys)
+	var rv []interface{}
+	c := rc.p.Get()
+	defer c.Close()
+	var err error
+	for _, key := range keys {
+		err = c.Send("GET", key)
+		if err != nil {
+			goto ERROR
+		}
+	}
+	if err = c.Flush(); err != nil {
+		goto ERROR
+	}
+	for i := 0; i < size; i++ {
+		if v, err := c.Receive(); err == nil {
+			rv = append(rv, v.([]byte))
+		} else {
+			rv = append(rv, err)
+		}
+	}
+	return rv
+ERROR:
+	rv = rv[0:0]
+	for i := 0; i < size; i++ {
+		rv = append(rv, nil)
+	}
+
+	return rv
+}
+
+// Set redis设置value
+func (rc *RedisPool) Set(key string, val interface{}) error {
+	var err error
+	if _, err = rc.do("SET", key, val); err != nil {
+		return err
+	}
+	return err
+}
+
+// Delete redis删除key
+func (rc *RedisPool) Delete(key string) error {
+	var err error
+	if _, err = rc.do("DEL", key); err != nil {
+		return err
+	}
+
+	return err
+}
+
+// IsExist 判断是否存在key
+func (rc *RedisPool) IsExist(key string) bool {
+	v, err := redis.Bool(rc.do("EXISTS", key))
+	if err != nil {
+		return false
+	}
+	return v
+}
+
+// Incr 原子操作添加数值
+func (rc *RedisPool) Incr(key string) int64 {
+	value, _ := redis.Int64(rc.do("INCRBY", key, int64(1)))
+	return value
+}
+
+// Decr 原子操作减去数值
+func (rc *RedisPool) Decr(key string) int64 {
+
+	value, _ := redis.Int64(rc.do("INCRBY", key, int64(-1)))
+	return value
+}
+
+func (rc *RedisPool) start(config string) error {
+
+	rc.conninfo = config
+	rc.dbNum = 0
+
+	rc.connectInit()
+
+	c := rc.p.Get()
+	defer c.Close()
+
+	return c.Err()
+}
+
+func (rc *RedisPool) connectInit() {
+	dialFunc := func() (c redis.Conn, err error) {
+		c, err = redis.Dial("tcp", rc.conninfo)
+		if err != nil {
+			return nil, err
+		}
+
+		_, selecterr := c.Do("SELECT", rc.dbNum)
+		if selecterr != nil {
+			c.Close()
+			return nil, selecterr
+		}
+		return
+	}
+	// initialize a new pool
+	rc.p = &redis.Pool{
+		MaxIdle:     3,
+		MaxActive:   20,
+		IdleTimeout: 300 * time.Second,
+		Dial:        dialFunc,
+	}
+}
+
+//NewRedis redis创建
+func NewRedis(config string) (adapter *RedisPool, err error) {
+
+	adapter = newRedis()
+	err = adapter.start(config)
+	if err != nil {
+		adapter = nil
+	}
+	return
+}
